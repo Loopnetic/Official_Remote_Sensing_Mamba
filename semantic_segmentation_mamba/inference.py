f205d763d2916e23c391bc2b8b7c024602086fc2
Simon Hurens <sh@arctus.ca>
2025-06-06 09:39:24 -0400

    Add inference on standalone image

--- semantic_segmentation_mamba/inference.py
+++ semantic_segmentation_mamba/inference.py
@@ -10,9 +10,10 @@ from tqdm import tqdm
 import PIL.Image
 import PIL
 import numpy as np
+import albumentations as A
 
 if ph.gpu_id is not None:
-    os.environ['CUDA_VISIBLE_DEVICES'] = "1"
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(ph.gpu_id)
 
 def save_prediction(prediction: torch.Tensor, filepath: str):
     tensor_softmax = torch.nn.Softmax(dim=0)(prediction)
@@ -24,7 +25,50 @@ def save_prediction(prediction: torch.Tensor, filepath: str):
     img_pil = PIL.Image.fromarray(np_argmax,mode='L')
     img_pil.save(f'{filepath}.png')
 
-def infer_net(dataset_name, load_checkpoint=True, save_prediction_img=False):
+def infer(images_folder, output_folder):
+    """
+    Function to infer a folder of images and save the predictions.
+    
+    Args:
+        images_folder (str): Path to the folder containing images.
+        output_folder (str): Path to the folder where predictions will be saved.
+    """
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Load the model
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    net = RSM_SS(num_classes=ph.num_classes, dims=ph.dims, depths=ph.depths, ssm_d_state=ph.ssm_d_state, ssm_dt_rank=ph.ssm_dt_rank, \
+               ssm_ratio=ph.ssm_ratio, mlp_ratio=ph.mlp_ratio)
+    net.to(device=device)
+
+    load_model = torch.load(ph.load, map_location=device)
+    net.load_state_dict(load_model)
+    net.eval()
+
+    for img_name in os.listdir(images_folder):
+
+        img_path = os.path.join(images_folder, img_name)
+        img = PIL.Image.open(img_path)
+        img = np.array(img).astype(np.uint8)
+        normalized = A.Normalize()(image=img)['image']
+        normalized = normalized.transpose(2, 0, 1)  # Convert to CxHxW format
+        normalized = np.expand_dims(normalized, axis=0)  # Add batch dimension
+        img_tensor = torch.tensor(normalized).float().to(device)
+        #print(f"img_tensor memory size: {img_tensor.element_size() * img_tensor.nelement() / 1024:.2f} KB")
+        with torch.no_grad():
+            pred = net(img_tensor)
+        save_prediction(np.squeeze(pred.detach().cpu()), os.path.join(output_folder, img_name.split('.')[0]))
+        logging.info(f'Prediction saved for {img_name} in {output_folder}')
+        
+        del img_tensor, pred  # clear batch variables from memory
+    
+    del net, load_model  # clear model variables from memory
+    torch.cuda.empty_cache()  # clear GPU memory
+    logging.info('Inference completed and memory cleared.')
+
+
+def infer_test_set(dataset_name, load_checkpoint=True, save_prediction_img=False):
     # 1. Create dataset
 
     test_dataset = BasicDataset(images_dir=f'{ph.root_dir}/{dataset_name}/test/image/',
@@ -54,7 +98,6 @@ def infer_net(dataset_name, load_checkpoint=True, save_prediction_img=False):
     load_model = torch.load(ph.load, map_location=device)
     net.load_state_dict(load_model)
     logging.info(f'Model loaded from {ph.load}')
-    torch.save(net.state_dict(), f'{dataset_name}_best_model.pth')
 
     metric_collection = MetricCollection({
         'accuracy': Accuracy(ignore_index=None, num_classes=ph.num_classes, multiclass=True, mdmc_average='global').to(device=device),
@@ -74,11 +117,11 @@ def infer_net(dataset_name, load_checkpoint=True, save_prediction_img=False):
             ss_preds = net(batch_img1)
             
             if save_prediction_img:
-                save_pred_directory = f"./{os.path.basename(ph.root_dir)}_{os.path.basename(ph.load).replace(".pth","")}")
+                save_pred_directory = f"./{os.path.basename(ph.root_dir)}_{os.path.basename(ph.load).replace('.pth','')}"
                 if not os.path.exists(save_pred_directory): os.makedirs(save_pred_directory)
 
-                for pred_img, name in zip(batch_img1, names):
-                    save_prediction(pred_img.detach().cpu(), os.path.join(save_pred_directory,name))
+                for pred_img, name in zip(ss_preds, names):
+                    save_prediction(pred_img.detach().cpu(), os.path.join(save_pred_directory, name))
 
             # Calculate and log other batch metrics
             metric_collection.update(ss_preds.float(), labels.long())
@@ -96,7 +139,9 @@ def infer_net(dataset_name, load_checkpoint=True, save_prediction_img=False):
 if __name__ == '__main__':
 
     try:
-        infer_net(dataset_name=ph.dataset_name, save_prediction_img=True)
+        #infer_test_set(dataset_name=ph.dataset_name, save_prediction_img=True)
+        infer(images_folder='/mnt/0_ARCTUS_Projects/18_MEI_SDB2/data/s2_gee_HBE/data_training_osw/good_bad_inputs/Good_sample/',
+              output_folder='/home/hurens/Documents/Official_Remote_Sensing_Mamba/simple_inference/output_on_val_set_oswnet')
     except KeyboardInterrupt:
         logging.info('Error')
         sys.exit(0)
